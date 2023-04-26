75ad0721b1eafaa67ba8da32d2c1b7ecb271eca9
Yuto Tanaka <karaage_nennen@icloud.com>
2023-04-26 17:53:23 +0900

    Fix function name

--- Invoke-PueueUpdateCargoPackage.ps1
+++ Invoke-PueueUpdateCargoPackage.ps1
@@ -1,4 +1,4 @@
-function Invoke-UpdateCargoPackage() {
+function Invoke-PueueUpdateCargoPackage() {
     $outdated_pkgs = cargo install-update -l  | Select-String "Yes" | ForEach-Object {$($_ -split(" "))[0]}
     foreach ($i in $outdated_pkgs) {
         pueue add -- "cargo install $i"
