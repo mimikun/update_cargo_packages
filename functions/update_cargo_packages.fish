888e6d83af4db65d0f92aaea1451fb3329ec5f0f
Yuto Tanaka <karaage_nennen@icloud.com>
2024-02-19 19:09:08 +0900

    Add: show packages to update

--- functions/update_cargo_packages.fish
+++ functions/update_cargo_packages.fish
@@ -1,5 +1,7 @@
 function update_cargo_packages --description 'Update cargo packages'
     set cargo_outdated_pkgs (cargo install-update -l | grep "Yes" | cut -d " " -f 1)
+    echo "Update these packages:"
+    echo "$cargo_outdated_pkgs"
     for i in $cargo_outdated_pkgs
         cargo install "$i"
     end
